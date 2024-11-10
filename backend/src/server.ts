96c33c40ba80e0fa32bd11384b975f91a5828eac
Dat Pham <phamtatdat1510@gmail.com>
2024-11-10 12:18:33 -0800

    Fixed port issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,5 +32,7 @@ app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3001, () => console.log('Server running on http://localhost:3001'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
 
